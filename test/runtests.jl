eb2b616324458dfffb61d61bef0a98723621547f
Tommy Hofmann <thofma@gmail.com>
2021-06-17 15:16:53 +0200

    Test something

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,24 +2,24 @@ using Oscar
 using Test
 using GAP
 
-include("Polytopes/runtests.jl")
-
-include("GAP/runtests.jl")
-
-include("Rings/integer-test.jl")
-include("Rings/rational-test.jl")
-include("Rings/mpoly-test.jl")
-include("Rings/affine-alghom-test.jl")
-include("Rings/mpoly-graded-test.jl")
-include("Rings/mpoly-local-test.jl")
-include("Rings/mpoly-nested-test.jl")
-include("Rings/MPolyQuo_test.jl")
-include("Rings/FractionalIdeal-test.jl")
-include("Rings/mpoly_affine_algebras_test.jl")
-include("Rings/slpolys-test.jl")
-include("Polymake/nmbthy-test.jl")
-include("Groups/runtests.jl")
-include("Rings/NumberField.jl")
+#include("Polytopes/runtests.jl")
+#
+#include("GAP/runtests.jl")
+#
+#include("Rings/integer-test.jl")
+#include("Rings/rational-test.jl")
+#include("Rings/mpoly-test.jl")
+#include("Rings/affine-alghom-test.jl")
+#include("Rings/mpoly-graded-test.jl")
+#include("Rings/mpoly-local-test.jl")
+#include("Rings/mpoly-nested-test.jl")
+#include("Rings/MPolyQuo_test.jl")
+#include("Rings/FractionalIdeal-test.jl")
+#include("Rings/mpoly_affine_algebras_test.jl")
+#include("Rings/slpolys-test.jl")
+#include("Polymake/nmbthy-test.jl")
+#include("Groups/runtests.jl")
+#include("Rings/NumberField.jl")
 
 # if Oscar.is_dev
 #   include("Examples/PlaneCurve-test.jl")
@@ -27,9 +27,9 @@ include("Rings/NumberField.jl")
 
 include("Rings/binomial-ideals-test.jl")
 
-include("Examples/galois-test.jl")
-include("Examples/ModStdQt-test.jl")
-include("Examples/ModStdNF-test.jl")
-
-include("Modules/FreeModules-graded-test.jl")
-include("Modules/GradedModules.jl")
+#include("Examples/galois-test.jl")
+#include("Examples/ModStdQt-test.jl")
+#include("Examples/ModStdNF-test.jl")
+#
+#include("Modules/FreeModules-graded-test.jl")
+#include("Modules/GradedModules.jl")
